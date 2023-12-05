e543386aed3994cbae5e69e1f5886b6b7011bf42
Matt Waldron <matt.waldron@bjss.com>
2023-12-05 10:09:43 +0000

    chore(cb2-9827): don't use versions for now

--- service-lambda/output.tf
+++ service-lambda/output.tf
@@ -1,6 +1,6 @@
 output "lambda_arn" {
   description = "The unique ARN of the lambda"
-  value = aws_lambda_function.service.qualified_invoke_arn
+  value = aws_lambda_function.service.invoke_arn
 }
 
 output "function_name" {
