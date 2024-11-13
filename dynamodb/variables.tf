44cc20f92d3944a0d514fde4f33a5dc61f9a997c
TaylorHalf <taylor.halfyard@dvsa.gov.uk>
2024-11-13 08:58:03 +0000

    Updated the variables

--- dynamodb/variables.tf
+++ dynamodb/variables.tf
@@ -1,5 +1,6 @@
 variable "service_name" {
-  type = string
+  type        = string
+  description = "The name of the service used as part of the DynamoDB table name."
 }
 
 variable "hash" {
@@ -7,16 +8,19 @@ variable "hash" {
     key  = string,
     type = string
   })
+  description = "Defines the hash key for the DynamoDB table, including the key name and type."
 }
+
 variable "range" {
   type = object({
     key  = string,
     type = string
   })
-  default = {
+  default     = {
     key  = null,
     type = null
   }
+  description = "Optional range key definition for the DynamoDB table, with the key name and type. Set to null if no range key is required."
 }
 
 variable "additional_attributes" {
@@ -24,33 +28,37 @@ variable "additional_attributes" {
     key  = string,
     type = string
   }))
-  default = []
+  description = "Additional non-key attributes to include in the table schema, specified as a list of objects with key names and types."
+  default     = []
 }
 
 variable "stream_enabled" {
-  type    = bool
-  default = false
+  type        = bool
+  description = "Enables DynamoDB Streams for the table when set to true, allowing capture of data changes."
+  default     = false
 }
 
 variable "global_secondary_indexes" {
   type = list(object({
-    name               = string,
+    name               = string
     hash_key           = string
     range_key          = string
     projection_type    = string
     non_key_attributes = list(string)
   }))
-  default = []
+  description = "A list of global secondary indexes to create, with each index specifying name, hash key, range key, projection type, and non-key attributes."
+  default     = []
 }
 
 variable "local_secondary_indexes" {
   type = list(object({
-    name               = string,
+    name               = string
     range_key          = string
     projection_type    = string
     non_key_attributes = list(string)
   }))
-  default = []
+  description = "A list of local secondary indexes to create, with each index specifying name, range key, projection type, and non-key attributes."
+  default     = []
 }
 
 variable "ttl" {
@@ -58,18 +66,21 @@ variable "ttl" {
     enabled        = bool,
     attribute_name = string
   }))
-  default = []
+  description = "Defines TTL (Time-to-Live) settings for the table, including a boolean to enable TTL and the attribute name used to store TTL values."
+  default     = []
+ 
 }
 
 variable "unique_name" {
-  type    = bool
-  default = false
+  type        = bool
+  description = "Determines if a unique name should be generated for the DynamoDB table using a random identifier."
+  default     = false
 }
 
 variable "billing_mode" {
   type        = string
-  default     = "PAY_PER_REQUEST"
   description = "DynamoDB Billing mode. Can be PROVISIONED or PAY_PER_REQUEST"
+  default     = "PAY_PER_REQUEST"
 }
 
 variable "enable_point_in_time_recovery" {
